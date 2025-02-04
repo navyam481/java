fd366dc1826e8d3ded160381c75a162c97755352
M NAVYASREE <navyam481@gmail.com>
2025-02-04 17:58:56 +0530

    day3

---
+++ wipro_java_project/src/com/wipro/java/interface1/Bird.java
@@ -0,0 +1,10 @@
+package com.wipro.java.interface1;
+
+public interface Bird 
+{
+	
+	/**
+	 * By default it is public abstract method
+	 */
+	void fly();
+}
---
+++ wipro_java_project/src/com/wipro/java/interface1/Drawable.java
@@ -0,0 +1,9 @@
+package com.wipro.java.interface1;
+
+public interface Drawable 
+{
+	/**
+	 * By default it is public abstract method
+	 */
+	void draw();
+}
