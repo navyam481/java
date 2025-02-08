262bf21adbfe94f981ead6b240a92d40c020824d
M NAVYASREE <navyam481@gmail.com>
2025-02-08 15:51:32 +0530

    day7

---
+++ spring-example/src/main/java/com/spring/sample/Test.java
@@ -0,0 +1,13 @@
+package com.spring.sample;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+	}
+}
